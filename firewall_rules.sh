82cb5ab5bb3a7ec5522e69c203ec41ab6c1bb317
kelthuzkingdom <kelthuzkingdom@gmail.com>
2026-02-06 10:38:04 +0000

    Create firewall_rules.sh

---
+++ firewall_rules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Basic firewall rules (iptables example)
+echo "Setting up firewall rules..."
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -j DROP
